66d4ee020f011f2d4439730907ef780ebc05d6e6
NitinTSutar <nitintsuthar67@gmail.com>
2025-03-17 20:04:23 +0530

    scroll-bar bug fixed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import MovieCart from "./MovieCart";
 
 const MovieList = ({ title, movies }) => {
+    const scrollContainerRef = useRef(null);
+
+    const scrollLeft = () => {
+        scrollContainerRef.current.scrollBy({
+            left: -300,
+            behavior: "smooth",
+        });
+    };
+
+    const scrollRight = () => {
+        scrollContainerRef.current.scrollBy({
+            left: 300,
+            behavior: "smooth",
+        });
+    };
+
     return (
         movies && (
-            <div className="pl-6">
+            <div className="pl-6 relative">
                 <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-                <div className="flex overflow-x-scroll no-scrollbar">
-                    <div className="flex gap-6 ">
-                        {movies.map((movie) => (
-                            <MovieCart
-                                key={movie.id}
-                                posterPath={movie.poster_path}
-                            />
-                        ))}
+                <div className="flex items-center">
+                    <button
+                        className="absolute left-0 z-10 bg-black/50 text-white p-2 rounded-full"
+                        onClick={scrollLeft}
+                    >
+                        {'<'}
+                    </button>
+                    <div
+                        className="flex overflow-x-scroll no-scrollbar"
+                        ref={scrollContainerRef}
+                        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+                    >
+                        <div className="flex gap-6" >
+                            {movies.map((movie) => (
+                                <MovieCart
+                                    key={movie.id}
+                                    posterPath={movie.poster_path}
+                                />
+                            ))}
+                        </div>
                     </div>
+                    <button
+                        className="absolute right-0 z-10 bg-black/50 text-white p-2 rounded-full"
+                        onClick={scrollRight}
+                    >
+                        {'>'}
+                    </button>
                 </div>
             </div>
         )
