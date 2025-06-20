166f50e7ca6006a1b4cc69576b648fe338d020c7
NitinTSutar <nitintsuthar67@gmail.com>
2025-06-21 02:51:31 +0530

    bug-fix

--- src/utilts/firebase.js
+++ src/utilts/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ-x-o3PcbSA-Hdw-xXYjgjN1Zq2kMMBo",
-    authDomain: "nefflix-76f46.firebaseapp.com",
-    projectId: "nefflix-76f46",
-    storageBucket: "nefflix-76f46.firebasestorage.app",
-    messagingSenderId: "964024548959",
-    appId: "1:964024548959:web:c25d680dc4a0ce7faae5ea",
-    measurementId: "G-ZC91HZL0D5",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
